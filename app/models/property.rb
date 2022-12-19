7cf700acd367e2b966a2a14f93307c6dedbb03fc
deepika joshi <upreti.deepika@gmail.com>
2022-12-19 15:44:39 +0800

    Added Validations to the property.rb file

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,15 @@
 class Property < ApplicationRecord
+  validates :name, presence: true
+  validates :headline, presence: true
+  validates :description, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
 end
+
+t.string "name"
+    t.string "headline"
+    t.text "description"
+    t.string "city"
+    t.string "state"
+    t.string "country"
