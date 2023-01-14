256a7a05514e8880ea19c1d38c2eb7748f2db70e
deepika joshi <upreti.deepika@gmail.com>
2023-01-14 22:04:28 +0800

    added reviews

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,4 @@
 class Listing < ApplicationRecord
   has_many :listing_images
+  has_many :reviews
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :listings, only: :show
-  get "listing_images", to: "listing_images#index"
-  get "reviews", to: "reviews#id"
+
+
+
 end
