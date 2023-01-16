57112bda9af0e4dfa4bb84d24df80f75dddc12d6
deepika joshi <upreti.deepika@gmail.com>
2023-01-16 10:14:44 +0800

    listing sub_categories

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,10 +2,19 @@ class ListingsController < ApplicationController
   def show
     # joining two or more tables using includes
     # @list_var has listing data and listing image data and review data for listing id = 1
-    @list_var = Listing.includes(:listing_images, :reviews)
+    @list_var = Listing.includes(:listing_images, :reviews, :listing_sub_categories)
                        .where(listings: { id: params[:id] })
     @secondary_images = @list_var[0].listing_images
     @reviews = @list_var[0].reviews
+    @sub_categories_array = @list_var[0].listing_sub_categories
+
+    # getting sub-categories
+    sub_cat_id = @sub_categories_array.map { |subcategory| subcategory.id }
+    @sub_categories = SubCategory.where('id in (?)', sub_cat_id)
+
+    puts @sub_categories.inspect
+
+
   end
 
   def index
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ApplicationRecord
   has_many :listing_images
   has_many :reviews
+  has_many :listing_sub_categories
 end
