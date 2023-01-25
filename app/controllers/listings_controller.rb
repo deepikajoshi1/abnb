309526f7ba7ad466a386491202418a1ad50afb6d
deepika joshi <upreti.deepika@gmail.com>
2023-01-25 16:29:12 +0800

    updated

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,21 +2,25 @@ class ListingsController < ApplicationController
   def show
     # joining two or more tables using includes
     # @list_var has listing data and listing image data and review data for listing id = 1
-    @list_var = Listing.includes(:listing_images, :reviews, :listing_sub_categories)
-                       .where(listings: { id: params[:id] })
+    # @list_var = Listing.includes(:listing_images, :reviews, :listing_sub_categories)
+    #                    .where(listings: { id: params[:id] })
+
+    @list_var = Listing.includes(:listing_images, :listing_sub_categories,
+                                 :reviews => :user)
+      .where(listings: { id: params[:id] })
+
     @secondary_images = @list_var[0].listing_images
     @reviews = @list_var[0].reviews
     @sub_categories_array = @list_var[0].listing_sub_categories
 
-    # getting sub-categories ids
+    # getting sub-categories ids 1,5,7
     sub_cat_id = @sub_categories_array.map { |subcategory| subcategory.id }
-    @sub_categories = SubCategory.where('id in (?)', sub_cat_id)
+    # select * from sub_category where id in [1,5,7];
+    @sub_categories = SubCategory.where("id in (?)", sub_cat_id)
 
-    puts @sub_categories.inspect
-
-    # iterate and get review id
-    # add where clause with where('id in (?)', review_id)
-    # Reviews.includes(:users). where (reviews : {id})
+    # x = [1,2,3]
+    # squarex = x.map{ |y| y*y }
+    # puts squarex.inspect
   end
 
   def index
